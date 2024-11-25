a6e2a2a6c4f4a1366a53b8b64df117fa04eb7d13
Davidd256 <davidd256@gmail.com>
2024-11-25 23:53:04 +0300

    Отредактированный текст.

--- OB01-1.py
+++ OB01-1.py
@@ -1,6 +1,5 @@
 # Пример реализации класса `Task` на Python
 #
-# ```python
 from datetime import datetime
 
 
@@ -19,10 +18,10 @@ class Task:
     def __repr__(self):
         status = "Выполнено" if self.completed else "Не выполнено"
         return (f"Задача: {self.description}, "
-                f"Дата создания: {self.creation_date.strftime('%Y-%m-%d %H:%M:%S')}, "
-                f"Срок выполнения: {self.due_date.strftime('%Y-%m-%d %H:%M:%S')}, "
+                f"Дата создания: {self.creation_date.strftime('%Y-%m-%d %H:%M')}, "
+                f"Срок выполнения: {self.due_date.strftime('%Y-%m-%d %H:%M')}, "
                 f"Статус: {status}, "
-                f"Дата фактического исполнения: {self.completion_date.strftime('%Y-%m-%d %H:%M:%S') if self.completion_date else 'Не выполнено'}")
+                f"Дата фактического исполнения: {self.completion_date.strftime('%Y-%m-%d %H:%M') if self.completion_date else 'Не выполнено'}")
 
 
 class TaskManager:
@@ -43,15 +42,18 @@ class TaskManager:
         current_tasks = [task for task in self.tasks if not task.completed]
         return sorted(current_tasks, key=lambda x: x.due_date)
 
+    def get_completed_tasks(self):
+        completed_tasks = [task for task in self.tasks if  task.completed]
+        return sorted(completed_tasks, key=lambda x: x.due_date)
 
 # Пример использования:
 if __name__ == "__main__":
     manager = TaskManager()
 
     # Добавление задач
-    manager.add_task("Сделать домашнее задание", datetime(2023, 10, 15))
-    manager.add_task("Купить продукты", datetime(2023, 10, 10))
-    manager.add_task("Подготовить отчет", datetime(2023, 10, 20))
+    manager.add_task("Сделать домашнее задание", datetime(2024, 12, 15))
+    manager.add_task("Купить продукты", datetime(2024, 12, 10))
+    manager.add_task("Подготовить отчет", datetime(2024, 12, 20))
 
     # Вывод текущих задач
     print("Текущие задачи:")
@@ -66,4 +68,10 @@ if __name__ == "__main__":
     print("\nТекущие задачи после выполнения одной из них:")
     current_tasks = manager.get_current_tasks()
     for index, task in enumerate(current_tasks):
+        print(f"{index}: {task}")
+
+    # Вывод выполненных задач после отметки
+    print("\nВыполненные задачи после выполнения одной из них:")
+    completed_tasks = manager.get_completed_tasks()
+    for index, task in enumerate(completed_tasks):
         print(f"{index}: {task}")
